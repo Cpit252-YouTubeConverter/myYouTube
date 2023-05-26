6df829913c4093c4fff752acadf8fd0478f9962b
waleed mohammed <wsaeedbamaqeen@stu.kau.edu.sa>
2023-05-26 20:40:32 +0300

    'cpit252project'

--- YouTube252Project/src/YoutubeDownloader.java
+++ YouTube252Project/src/YoutubeDownloader.java
@@ -7,19 +7,10 @@ public class YoutubeDownloader {
     public void downloadVideo(String url) {
         try {
             String output_path = "C:\\Users\\Best\\Downloads"; // replace with your directory
-            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\main.py", url, output_path);
-
-            pb.redirectErrorStream(true); // Redirects stderr to stdout
+            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\CPIT252\\myYouTube\\video_downloader\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\CPIT252\\myYouTube\\video_downloader\\main.py", url, output_path);
             Process p = pb.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = p.waitFor();
-            System.out.println("\nExitedwith error code : " + exitCode);
+            p.waitFor();
+            System.out.println("Video downloaded successfully!");
         } catch (Exception e) {
             System.out.println("Error downloading video: " + e.getMessage());
         }
@@ -28,7 +19,7 @@ public class YoutubeDownloader {
     public void downloadAudio(String url) {
         try {
             String output_path = "C:\\Users\\Best\\Downloads"; // replace with your directory
-            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\audi.py", url, output_path);
+            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\CPIT252\\myYouTube\\video_downloader\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\CPIT252\\myYouTube\\video_downloader\\audi.py", url, output_path);
             Process p = pb.start();
             p.waitFor();
             System.out.println("Audio downloaded successfully!");
--- video_downloader/audi.py
+++ video_downloader/audi.py
@@ -1,18 +1,18 @@
 from pytube import YouTube
 from moviepy.editor import *
 
-def download_video_and_convert_to_audio(url, output_path):
+def download_audio(url, output_path):
     yt = YouTube(url)
-    stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
-    video_file_path = stream.download(output_path)
+    audio_streams = yt.streams.filter(only_audio=True)
+    audio_stream = audio_streams.first()
+    audio_file_path = audio_stream.download(output_path)
 
-
-    video_clip = VideoFileClip(video_file_path)
-    audio_file_path = video_file_path.replace(".mp4", ".mp3")
-    video_clip.audio.write_audiofile(audio_file_path)
+    audio_clip = AudioFileClip(audio_file_path)
+    final_audio_file_path = audio_file_path.replace(".webm", ".mp3")
+    audio_clip.write_audiofile(final_audio_file_path)
 
 if __name__ == "__main__":
     import sys
     url = sys.argv[1]
     output_path = sys.argv[2]
-    download_video_and_convert_to_audio(url, output_path)
+    download_audio(url, output_path)
