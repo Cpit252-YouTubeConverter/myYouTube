87edd8716be39b48a227478ff6577e7969605366
waleed mohammed <wsaeedbamaqeen@stu.kau.edu.sa>
2023-05-26 16:11:22 +0300

    'after_update'

--- YouTube252Project/src/YoutubeDownloader.java
+++ YouTube252Project/src/YoutubeDownloader.java
@@ -7,7 +7,7 @@ public class YoutubeDownloader {
     public void downloadVideo(String url) {
         try {
             String output_path = "C:\\Users\\Best\\Downloads"; // replace with your directory
-            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\cpit252pro\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\cpit252pro\\main.py", url, output_path);
+            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\main.py", url, output_path);
 
             pb.redirectErrorStream(true); // Redirects stderr to stdout
             Process p = pb.start();
@@ -28,7 +28,7 @@ public class YoutubeDownloader {
     public void downloadAudio(String url) {
         try {
             String output_path = "C:\\Users\\Best\\Downloads"; // replace with your directory
-            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\cpit252pro\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\cpit252pro\\audi.py", url, output_path);
+            ProcessBuilder pb = new ProcessBuilder("C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\venv\\Scripts\\python.exe", "C:\\Users\\Best\\Documents\\CPIT252Project\\myYouTube\\video_downloader\\audi.py", url, output_path);
             Process p = pb.start();
             p.waitFor();
             System.out.println("Audio downloaded successfully!");
